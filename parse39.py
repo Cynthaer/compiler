5aca108f1a707e6e247ad7f793ebbbea8f40f499
Cynthaer <Cynthaer@users.noreply.github.com>
2016-09-26 16:29:16 -0500

    sync from work laptop

--- parse39.py
+++ parse39.py
@@ -25,19 +25,49 @@ class Parser(object):
     def parse_Tpr(self):
         c = self.peek()
         
-        first_Tpr = ('a', 'b', '$')
+        first_Tpr = {'a', 'b', '$'}
         
+        # unnecessary check, but convenient
         if c not in first_Tpr:
             raise ValueError('No production found for {} in T\''.format(c))
+        
         # T' -> T$
-        self.parseT()
+        self.parse_T()
         self.eat('$')
     
-    def parseT(self):
+    def parse_T(self):
         c = self.peek()
         
+        follow_T = {'$', 'c'}
+        
         # T -> R
+        first_R = {'b'}
+        nullable_R = True
+        
+        if c in first_R or (nullable_R and c in follow_T):
+            self.parse_R()
+            return
+        
         # T -> aTc
+        first_aTc = {'a'}
+        nullable_aTc = False
+        
+        if c in first_aTc or (nullable_aTc and c in follow_T):
+            self.eat('a')
+            self.parse_T()
+            self.eat('c')
+            return
+    
+    def parse_R(self):
+        c = self.peek()
+        
+        follow_R = {'$', 'c', 'b'}
+        
+        # R ->
+        
+        first_ε = set()
+        
+        # R -> bR
 
 def main():
     p = Parser('c')
